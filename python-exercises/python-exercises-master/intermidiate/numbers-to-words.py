0624cf925f73dfc5d93aab9818b712b756407933
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-10-26 14:38:27 +0000

    numbers to words code using library

---
+++ python-exercises/python-exercises-master/intermidiate/numbers-to-words.py
@@ -0,0 +1,18 @@
+
+### to use the library use pip install num2words
+
+
+
+from num2words  import num2words
+
+
+
+
+
+
+if __name__ == '__main__':
+
+   jeff = num2words(399904)
+   print(jeff)
+
+
