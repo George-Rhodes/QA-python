8347975b876f6c5460fac0238da3d2191b2ad9bf
George-Rhodes <george_rhodes@hotmail.co.uk>
2020-10-29 10:18:47 +0000

    adding files from other repo

---
+++ python-exercises/tests/test_vowels.py
@@ -0,0 +1,13 @@
+import pytest
+from applications import vowels
+
+
+
+def test_word_vowels():
+    assert vowels.vowels('sauceybegger') == 5
+
+def test_predetermined_vowels():
+    assert vowels.vowels('aaaauuuuoooo') == 12
+
+def test_novowels():
+    assert vowels.vowels('crypt') == 0
